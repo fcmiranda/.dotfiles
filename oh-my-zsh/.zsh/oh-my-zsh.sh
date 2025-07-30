5be400b3e047d5d7307eef1cca34bdeb39b7fbd2
fcmiranda <fecmbr@gmail.com>
2025-07-30 06:58:32 -0300

    fix: format and organize Oh My Zsh configuration

--- oh-my-zsh/.zsh/oh-my-zsh.sh
+++ oh-my-zsh/.zsh/oh-my-zsh.sh
@@ -1,8 +1,9 @@
-  # Oh My Zsh configuration
-  export ZSH="$HOME/.oh-my-zsh"
-  plugins=(
+# Oh My Zsh configuration
+export ZSH="$HOME/.oh-my-zsh"
+plugins=(
     git
-  zsh-autosuggestions
-  you-should-use
+    zsh-autosuggestions
+    you-should-use
+    zsh-syntax-highlighting
 )
-  [[ -f $ZSH/oh-my-zsh.sh ]] && source $ZSH/oh-my-zsh.sh
+[[ -f $ZSH/oh-my-zsh.sh ]] && source $ZSH/oh-my-zsh.sh

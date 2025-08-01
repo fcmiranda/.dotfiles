c0165cbc7a66282d1c4f22159cf521bb47710c7e
fcmiranda <fecmbr@gmail.com>
2025-07-31 23:12:48 -0300

    feat: add initial starship prompt configuration

--- oh-my-zsh/.zsh/oh-my-zsh.sh
+++
@@ -1,9 +0,0 @@
-# Oh My Zsh configuration
-export ZSH="$HOME/.oh-my-zsh"
-plugins=(
-    git
-    zsh-autosuggestions
-    you-should-use
-  zsh-syntax-highlighting
-)
-[[ -f $ZSH/oh-my-zsh.sh ]] && source $ZSH/oh-my-zsh.sh

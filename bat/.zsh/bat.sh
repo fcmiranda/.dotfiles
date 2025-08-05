a8ddc7f6b0646f3139e3cc3f30fc9a244d911ecf
felipe <felip.3@hotmail.com>
2025-08-05 20:06:38 -0300

    feat: update bat and fzf installation timestamps; add oh-my-zsh plugins

--- bat/.zsh/bat.sh
+++ bat/.zsh/bat.sh
@@ -1,3 +1,4 @@
-  alias cat='bat'
-  alias less='bat'
-  alias ccat='bat'
+# Enhanced cat with syntax highlighting
+alias cat='bat'
+alias less='bat'
+export BAT_THEME="TwoDark"
--- fzf/.zsh/fzf.sh
+++ fzf/.zsh/fzf.sh
@@ -1,9 +1,9 @@
-  # Configure fzf key bindings and completion
-  [[ -f ~/.fzf.zsh ]] && source ~/.fzf.zsh
-  # Set default fzf options
-  export FZF_DEFAULT_OPTS='--height 40% --layout=reverse --border'
-  # Use fd with fzf if available
-  if command -v fd > /dev/null; then
-    export FZF_DEFAULT_COMMAND='fd --type f --hidden --follow --exclude .git'
-    export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
-  fi
+# Configure fzf key bindings and completion
+[[ -f ~/.fzf.zsh ]] && source ~/.fzf.zsh
+# Set default fzf options
+export FZF_DEFAULT_OPTS='--height 40% --layout=reverse --border'
+# Use fd with fzf if available
+if command -v fd > /dev/null; then
+  export FZF_DEFAULT_COMMAND='fd --type f --hidden --follow --exclude .git'
+  export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
+fi

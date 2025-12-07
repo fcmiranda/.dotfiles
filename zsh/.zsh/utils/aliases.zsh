7ae2de7ae1e92dd2597b05013ce45cb013c3b89d
Felipe <fecmbr@gmail.com>
2025-12-07 16:26:45 -0300

    Update Walker theme configuration and add custom styles

--- zsh/.zsh/utils/aliases.zsh
+++ zsh/.zsh/utils/aliases.zsh
@@ -1,4 +1,5 @@
 alias waybar-restart='killall waybar && hyprctl dispatch exec waybar'
+alias walker-restart='pkill -f walker && walker &'
 alias reload="source ~/.zshrc"
 alias omc='/usr/bin/git --git-dir=/home/fecavmi/.omc --work-tree=/home/fecavmi'
 alias code.omc='GIT_DIR=/home/fecavmi/.omc GIT_WORK_TREE=/home/fecavmi code /home/fecavmi'

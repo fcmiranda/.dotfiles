724b89fd671e3f3d9f91a4369b9674e3c6da068a
Felipe <fecmbr@gmail.com>
2026-01-16 22:41:21 -0300

    feat: add baleia.nvim integration and scrollback alias

--- zsh/.zsh/utils/aliases.zsh
+++ zsh/.zsh/utils/aliases.zsh
@@ -4,3 +4,5 @@ alias reload="source ~/.zshrc"
 alias omc='/usr/bin/git --git-dir=/home/fecavmi/.omc --work-tree=/home/fecavmi'
 alias code.omc='GIT_DIR=/home/fecavmi/.omc GIT_WORK_TREE=/home/fecavmi code /home/fecavmi'
 alias install-packages='~/omc/install/install.zsh'
+alias clear="clear && tmux clear-history"
+alias scrollback='tmux capture-pane -epS - > /tmp/tmux_scrollback.ansi && nvim -c "BaleiaColorize" -c "normal G" /tmp/tmux_scrollback.ansi'

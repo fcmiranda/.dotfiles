8f9f9d2998af752e3983200cb25b043946035602
Felipe <fecmbr@gmail.com>
2026-01-17 12:33:51 -0300

    fix: restore key bindings to prevent vi mode activation

--- zsh/.zsh/utils/binds.zsh
+++ zsh/.zsh/utils/binds.zsh
@@ -1,3 +1,11 @@
 # # binds to use emacs mode (disables vi mode)
 # bindkey -e
 # bindkey '\e' vi-cmd-mode
+
+# Prevent Delete key from activating vi normal mode
+bindkey '^[[3~' delete-char  # Standard Delete key
+bindkey '^?' backward-delete-char  # Backspace
+
+# Prevent Ctrl+Arrow keys from activating vi normal mode
+bindkey '^[[1;5D' backward-word  # Ctrl+Left Arrow
+bindkey '^[[1;5C' forward-word   # Ctrl+Right Arrow

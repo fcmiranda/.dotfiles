505e91ff7c5cac3ff4b44a8aa9ccda45b36c631f
Felipe <fecmbr@gmail.com>
2026-01-18 14:51:46 -0300

    feat: refactor zsh-transient-prompt loading to ensure compatibility with zsh-vi-mode

--- zsh-plugins/.zsh/plugins/zsh-transient-prompt.zsh
+++ zsh-plugins/.zsh/plugins/zsh-transient-prompt.zsh
@@ -1,6 +1,17 @@
 # Source zsh-transient-prompt plugin
-if [ -f "${HOME}/.zsh-plugins/zsh-transient-prompt/transient-prompt.plugin.zsh" ]; then
-    source "${HOME}/.zsh-plugins/zsh-transient-prompt/transient-prompt.plugin.zsh"
+# When using zsh-vi-mode, transient-prompt must be loaded after zvm initialization
+# because zvm overrides zle-line-finish widget
 
-    TRANSIENT_PROMPT_TRANSIENT_PROMPT='% '
+_load_transient_prompt() {
+    if [ -f "${HOME}/.zsh-plugins/zsh-transient-prompt/transient-prompt.plugin.zsh" ]; then
+        source "${HOME}/.zsh-plugins/zsh-transient-prompt/transient-prompt.plugin.zsh"
+        TRANSIENT_PROMPT_TRANSIENT_PROMPT='% '
+    fi
+}
+
+# Check if zsh-vi-mode is loaded and use its hook, otherwise load directly
+if (( ${+functions[zvm_after_init]} )) || [[ -n "${zvm_after_init_commands+x}" ]]; then
+    zvm_after_init_commands+=(_load_transient_prompt)
+else
+    _load_transient_prompt
 fi

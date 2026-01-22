ffe2d04a2f1e08646ecc55f75a4c6063eb000b7a
Felipe <fecmbr@gmail.com>
2026-01-22 12:57:50 -0300

    feat: update zsh-autosuggestions plugin to support vi and emacs key bindings

--- zsh-plugins/.zsh/plugins/zsh-autosuggestions.zsh
+++ zsh-plugins/.zsh/plugins/zsh-autosuggestions.zsh
@@ -2,5 +2,7 @@
 if [ -f "${HOME}/.zsh-plugins/zsh-autosuggestions/zsh-autosuggestions.zsh" ]; then
     source "${HOME}/.zsh-plugins/zsh-autosuggestions/zsh-autosuggestions.zsh"
 
-    bindkey '\t\t' autosuggest-accept
+    # Ensure the binding works in vi insert mode (and emacs mode as a fallback)
+    bindkey -M viins '^I' autosuggest-accept
+    bindkey -M emacs '^I' autosuggest-accept
 fi

a6b0ab2083ab393dcea629cd60f1a45b81dea57a
Felipe <fecmbr@gmail.com>
2026-03-06 22:51:58 -0300

    feat(cava): optimize bar animation and update configuration for improved performance

--- waybar/.config/waybar/modules/cava.sh
+++ waybar/.config/waybar/modules/cava.sh
@@ -1,8 +1,47 @@
-#!/usr/bin/env bash
+#!/bin/bash
+# /* ---- 💫 https://github.com/JaKooLit 💫 ---- */  ##
+# Not my own work. This was added through Github PR. Credit to original author
 
-cava -p "$HOME/.config/cava/waybar.ini" | while IFS= read -r line; do
-    if playerctl -p spotify status 2>/dev/null | grep -q "Playing"; then
-        echo "$line" | sed 's/;//g;s/0/▁/g;s/1/▁/g;s/2/▂/g;s/3/▃/g;s/4/▄/g;s/5/▅/g;s/6/▆/g;s/7/▇/g;s/8/█/g'
+#----- Optimized bars animation without much CPU usage increase --------
+bar="▁▂▃▄▅▆▇█"
+dict="s/;//g"
+
+# Calculate the length of the bar outside the loop
+bar_length=${#bar}
+
+# Create dictionary to replace char with bar
+for ((i = 0; i < bar_length; i++)); do
+    dict+=";s/$i/${bar:$i:1}/g"
+done
+
+# Create cava config
+config_file="/tmp/bar_cava_config"
+cat >"$config_file" <<EOF
+[general]
+# Older systems show significant CPU use with default framerate
+# Setting maximum framerate to 30  
+# You can increase the value if you wish
+framerate = 30
+bars = 10
+
+[input]
+method = pulse
+source = auto
+
+[output]
+method = raw
+raw_target = /dev/stdout
+data_format = ascii
+ascii_max_range = 7
+EOF
+
+# Kill cava if it's already running
+pkill -f "cava -p $config_file"
+
+# Read stdout from cava, show bars only when music is playing
+cava -p "$config_file" | sed -u "$dict" | while IFS= read -r line; do
+    if playerctl status 2>/dev/null | grep -q "Playing"; then
+        echo "$line"
     else
         echo ""
     fi

3502be4c2c01e3160328cd627673539e400a3788
felipe cavalcante miranda <fecmbr@gmail.com>
2026-03-10 22:32:48 -0300

    feat(zsh): add alias for easier access to current directory git repository

--- zsh/.zsh/utils/aliases.zsh
+++ zsh/.zsh/utils/aliases.zsh
@@ -3,6 +3,7 @@ alias walker-restart='pkill -f walker && walker &'
 alias reload="source ~/.zshrc"
 alias omc='/usr/bin/git --git-dir=/home/fecavmi/.omc --work-tree=/home/fecavmi'
 alias code.omc='GIT_DIR=/home/fecavmi/.omc GIT_WORK_TREE=/home/fecavmi code /home/fecavmi'
+alias code.='GIT_DIR=$PWD/.git GIT_WORK_TREE=$PWD code .'
 alias install-packages='~/omc/install/install.zsh'
 alias clear="clear && [ -n \"\$TMUX\" ] && tmux clear-history"
 alias scrollback='tmux capture-pane -epS - > /tmp/tmux_scrollback.ansi && nvim -c "BaleiaColorize" -c "normal G" /tmp/tmux_scrollback.ansi'

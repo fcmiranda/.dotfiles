6535922dc0ce3a8b4e7ef8bf6144708ef46cb7a9
felipe <fecmbr@gmail.com>
2026-04-08 07:38:49 -0300

    fix: update adopt alias to correct script path

--- zsh/.zsh/utils/aliases.zsh
+++ zsh/.zsh/utils/aliases.zsh
@@ -7,4 +7,4 @@ alias code.='GIT_DIR=$PWD/.git GIT_WORK_TREE=$PWD code .'
 alias install-packages='~/omc/install/install.zsh'
 alias clear="clear && [ -n \"\$TMUX\" ] && tmux clear-history"
 alias scrollback='tmux capture-pane -epS - > /tmp/tmux_scrollback.ansi && nvim -c "BaleiaColorize" -c "normal G" /tmp/tmux_scrollback.ansi'
-alias adopt='bash $HOME/.dotfiles/scripts/stow-adopt-path.sh'
+alias adopt='bash $HOME/.dotfiles/.shell/sh/stow-adopt-path.sh'

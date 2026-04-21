e0c168ded939fc2515e5e5e6a188ceab508f7d98
felipe <fecmbr@gmail.com>
2026-04-21 12:05:09 -0300

    refactor(tmux): remove FloaX-style bindings and simplify popup title

--- tmux/.config/tmux/opencode-bell-popup.sh
+++ tmux/.config/tmux/opencode-bell-popup.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env bash
-FLOAX_DIR="$HOME/.tmux/plugins/tmux-floax/scripts"
-
 BORDER_COLOR="magenta"
 TEXT_COLOR="blue"
 WIDTH="90%"
@@ -22,21 +20,7 @@ sess=$(tmux display-message -t "$pane" -p '#S' 2>/dev/null)
 win_idx=$(tmux display-message -t "$pane" -p '#I' 2>/dev/null)
 win_name=$(tmux display-message -t "$pane" -p '#W' 2>/dev/null)
 
-TITLE=" $sess › $win_name  │  C-M-s 󰘕  C-M-b 󰁌  C-M-f 󰊓  C-M-r 󰑓"
-
-# Set FloaX-style resize bindings
-tmux bind -n C-M-s run "$FLOAX_DIR/zoom-options.sh in"
-tmux bind -n C-M-b run "$FLOAX_DIR/zoom-options.sh out"
-tmux bind -n C-M-f run "$FLOAX_DIR/zoom-options.sh full"
-tmux bind -n C-M-r run "$FLOAX_DIR/zoom-options.sh reset"
-tmux bind -n C-M-e run "$FLOAX_DIR/embed.sh embed"
-tmux bind -n C-M-d run "$FLOAX_DIR/zoom-options.sh lock"
-tmux bind -n C-M-u run "$FLOAX_DIR/zoom-options.sh unlock"
-
-# Hide status bar; restore + unbind resize keys on popup close
-tmux set-option -t "$sess" status off
-tmux set-hook -t "$sess" client-detached \
-  "set-option status on ; unbind -n C-M-s ; unbind -n C-M-b ; unbind -n C-M-f ; unbind -n C-M-r ; unbind -n C-M-e ; unbind -n C-M-d ; unbind -n C-M-u ; set-hook -u client-detached"
+TITLE=" $sess › $win_name  │  prefix+q close"
 
 tmux popup \
   -S "fg=$BORDER_COLOR" \

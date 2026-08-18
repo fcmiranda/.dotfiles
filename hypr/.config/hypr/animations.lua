16fbc0fdc454dfc768ac9e63597968d61fa6dec2
felipe cavalcante miranda <fecmbr@gmail.com>
2026-08-17 23:12:59 -0300

    fix(hypr): set fast preset as active animations

--- hypr/.config/hypr/animations.lua
+++ hypr/.config/hypr/animations.lua
@@ -1,13 +1,16 @@
--- Minimal & Instant Style
+-- Fast & Snappy Style
 
 hl.config({ animations = { enabled = true } })
 
-hl.curve("linear", { type = "bezier", points = { { 0.0, 0.0 }, { 1.0, 1.0 } } })
+hl.curve("ease", { type = "bezier", points = { { 0.25, 0.1 }, { 0.25, 1.0 } } })
+hl.curve("overshot", { type = "bezier", points = { { 0.13, 0.99 }, { 0.29, 1.05 } } })
+hl.curve("liner", { type = "bezier", points = { { 1, 1 }, { 1, 1 } } })
 
-hl.animation({ leaf = "windows", enabled = true, speed = 1, bezier = "linear", style = "popin" })
-hl.animation({ leaf = "windowsOut", enabled = true, speed = 1, bezier = "linear", style = "popin" })
-hl.animation({ leaf = "windowsMove", enabled = true, speed = 1, bezier = "linear" })
-hl.animation({ leaf = "layers", enabled = true, speed = 1, bezier = "linear" })
-hl.animation({ leaf = "fade", enabled = true, speed = 1, bezier = "linear" })
-hl.animation({ leaf = "border", enabled = false })
-hl.animation({ leaf = "workspaces", enabled = true, speed = 1, bezier = "linear", style = "fade" })
+hl.animation({ leaf = "windows", enabled = true, speed = 2, bezier = "overshot", style = "popin 90%" })
+hl.animation({ leaf = "windowsOut", enabled = true, speed = 2, bezier = "ease", style = "slide bottom" })
+hl.animation({ leaf = "windowsMove", enabled = true, speed = 2, bezier = "overshot", style = "slide" })
+hl.animation({ leaf = "layers", enabled = true, speed = 2, bezier = "ease", style = "fade" })
+hl.animation({ leaf = "fade", enabled = true, speed = 1, bezier = "ease" })
+hl.animation({ leaf = "border", enabled = true, speed = 1, bezier = "liner" })
+hl.animation({ leaf = "borderangle", enabled = true, speed = 100, bezier = "liner", style = "loop" })
+hl.animation({ leaf = "workspaces", enabled = true, speed = 2, bezier = "overshot", style = "slide" })

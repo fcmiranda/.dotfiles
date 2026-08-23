224236f4ee020cd3500629d3e8c5e14a0c570b43
felipe <fecmbr@gmail.com>
2026-08-23 13:40:28 -0300

    refactor: update monitor scaling, reorganize bar layout, and replace custom Hyprland configuration with a standard stub

--- hypr/.config/hypr/monitors.lua
+++ hypr/.config/hypr/monitors.lua
@@ -1,11 +1,10 @@
 -- See https://wiki.hypr.land/Configuring/Basics/Monitors/
 -- List current monitors and supported resolutions with: hyprctl monitors all
 
-local omarchy_gdk_scale = 1
-local omarchy_monitor_scale = 1
+local omarchy_gdk_scale = 2
+local omarchy_monitor_scale = "auto"
 
 hl.env("GDK_SCALE", tostring(omarchy_gdk_scale))
-hl.monitor({ output = "eDP-1", mode = "1366x768@60", position = "0x0", scale = 1 })
 hl.monitor({ output = "", mode = "preferred", position = "auto", scale = omarchy_monitor_scale })
 
 -- Configure a specific monitor.

282b844d99fa229ab3b50b1f90667422431a00b3
felipe <fecmbr@gmail.com>
2026-08-23 21:12:22 -0300

    refactor: update Hyprland animations to use HyDE project curves and settings

--- hypr/.config/hypr/animations.lua
+++ hypr/.config/hypr/animations.lua
@@ -1,16 +1,18 @@
--- Fast & Snappy Style
+-- HyDE Project (Hyprland Desktop Environment) Style
 
 hl.config({ animations = { enabled = true } })
 
-hl.curve("ease", { type = "bezier", points = { { 0.25, 0.1 }, { 0.25, 1.0 } } })
-hl.curve("overshot", { type = "bezier", points = { { 0.13, 0.99 }, { 0.29, 1.05 } } })
+hl.curve("wind", { type = "bezier", points = { { 0.05, 0.9 }, { 0.1, 1.05 } } })
+hl.curve("winIn", { type = "bezier", points = { { 0.1, 1.1 }, { 0.1, 1.1 } } })
+hl.curve("winOut", { type = "bezier", points = { { 0.3, -0.3 }, { 0, 1 } } })
 hl.curve("liner", { type = "bezier", points = { { 1, 1 }, { 1, 1 } } })
 
-hl.animation({ leaf = "windows", enabled = true, speed = 2, bezier = "overshot", style = "popin 90%" })
-hl.animation({ leaf = "windowsOut", enabled = true, speed = 2, bezier = "ease", style = "slide bottom" })
-hl.animation({ leaf = "windowsMove", enabled = true, speed = 2, bezier = "overshot", style = "slide" })
-hl.animation({ leaf = "layers", enabled = true, speed = 2, bezier = "ease", style = "fade" })
-hl.animation({ leaf = "fade", enabled = true, speed = 1, bezier = "ease" })
+hl.animation({ leaf = "windows", enabled = true, speed = 4, bezier = "wind", style = "slide" })
+hl.animation({ leaf = "windowsIn", enabled = true, speed = 4, bezier = "winIn", style = "slide" })
+hl.animation({ leaf = "windowsOut", enabled = true, speed = 3, bezier = "winOut", style = "slide" })
+hl.animation({ leaf = "windowsMove", enabled = true, speed = 4, bezier = "wind", style = "slide" })
+hl.animation({ leaf = "layers", enabled = true, speed = 4, bezier = "wind", style = "slide" })
+hl.animation({ leaf = "fade", enabled = true, speed = 10, bezier = "default" })
 hl.animation({ leaf = "border", enabled = true, speed = 1, bezier = "liner" })
 hl.animation({ leaf = "borderangle", enabled = true, speed = 100, bezier = "liner", style = "loop" })
-hl.animation({ leaf = "workspaces", enabled = true, speed = 2, bezier = "overshot", style = "slide" })
+hl.animation({ leaf = "workspaces", enabled = true, speed = 5, bezier = "wind" })

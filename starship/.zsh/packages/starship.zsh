addc5f1573b872661460b1b5baacf42a8c66d7e8
felipe <fecmbr@gmail.com>
2026-08-23 21:30:23 -0300

    fix(starship): move theme template to themed folder and update state path

--- starship/.zsh/packages/starship.zsh
+++ starship/.zsh/packages/starship.zsh
@@ -1,3 +1,3 @@
 export PATH="$HOME/.config/omarchy/bin:$PATH"
-export STARSHIP_CONFIG=$HOME/.config/omarchy/current/theme/starship.toml
+export STARSHIP_CONFIG=$HOME/.local/state/omarchy/current/theme/starship.toml
 eval "$(starship init zsh)"
